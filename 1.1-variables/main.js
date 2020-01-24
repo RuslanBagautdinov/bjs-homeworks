3db0bd4267e26d38aef7df4dd107eec644360881
Ruslan Baga <ba-ru@yandex.ru>
2020-01-24 19:34:07 +0500

    homework 1.1

---
+++ 1.1-variables/main.js
@@ -0,0 +1,15 @@
+let algebra = 5;
+let geography = 3;
+let physics = 4;
+let avarageGrade = (algebra + geography + physics) / 3;
+console.log(avarageGrade);
+
+let myName = "Vasya";
+let message = `Привет, мир! Меня зовут ${myName}`;
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
